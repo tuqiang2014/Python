6a940ee09c5733c7291226f735a9d97fa3ced998
Zhouyunlai <1102279384@qq.com>
2017-07-23 18:31:56 +0800

    Create ex1.py
    Hello world

---
+++ ex1.py
@@ -0,0 +1,8 @@
+# -*- coding：utf-8 -*-
+print("Hello world!")
+print("Hello Again")
+print("I like typing this.")
+print("This is fun.")
+print("YaY! Printing")
+print("I'd much rather you 'not'.")
+print('I "said" do not touch this')
